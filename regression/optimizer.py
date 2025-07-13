1ba9e26cb1372ba71b58cbf185b6013b41bdb8f8
lostinentropy <lostinentropy@posteo.org>
2025-07-12 21:36:40 -0700

    added ElasticNet

--- regression/optimizer.py
+++ regression/optimizer.py
@@ -4,14 +4,18 @@ import numpy as np
 from .models.compressive import CompressiveLinearModel
 from .utils import prox
 
+
 class Optimizer:
     def step(self, _model: Any, _params: Any): ...
 
 
 class SubGradientDescent(Optimizer):
-    def __init__(self, learning_rate: float = 0.01, lam: float = 0.01):
+    def __init__(
+        self, learning_rate: float = 0.01, lam_1: float = 0.01, lam_2: float = 0.01
+    ):
         self.learning_rate: float = learning_rate
-        self.lam: float = lam
+        self.lam_1: float = lam_1
+        self.lam_2: float = lam_2
 
     @override
     def step(
@@ -22,7 +26,11 @@ class SubGradientDescent(Optimizer):
         X = params["X"]
         y = params["y"]
         residual = model(X) - y
-        grad = X.T @ residual / X.shape[0] + self.lam * np.sign(model.w)
+        grad = (
+            X.T @ residual / X.shape[0]
+            + self.lam_1 * np.sign(model.w)
+            + 2 * self.lam_2 * model.w
+        )
         model.w -= self.learning_rate * grad
 
 
@@ -57,3 +65,21 @@ class CoordinateDescent(Optimizer):
         params: dict[str, np.typing.NDArray[np.float64]],
     ):
         pass
+
+
+class ElasticNet(Optimizer):
+    def __init__(self, learning_rate: float = 0.01, lam: float = 0.01):
+        self.learning_rate: float = learning_rate
+        self.lam: float = lam
+
+    @override
+    def step(
+        self,
+        model: CompressiveLinearModel,
+        params: dict[str, np.typing.NDArray[np.float64]],
+    ):
+        X = params["X"]
+        y = params["y"]
+        residual = model(X) - y
+        grad = X.T @ residual / X.shape[0] + self.lam * np.sign(model.w)
+        model.w -= self.learning_rate * grad
